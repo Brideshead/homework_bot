c67133b39b5e71dabd6d2aee9e98f8ba9dddf498
Brideshead <alexander.bogoevich@gmail.com>
2022-12-10 23:15:34 +0100

    bot

--- homework.py
+++ homework.py
@@ -1,17 +1,22 @@
 import logging
 import os
-import requests
-import sys
-import telegram
 import time
-from typing import Dict
+from http import HTTPStatus
+from pathlib import Path
+from typing import Dict, List
+
+import requests
+import telegram
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 
-RETRY_PERIOD = 600
+RETRY_PERIOD = 600  # 10 минут * 60 секунд
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -23,14 +28,11 @@ HOMEWORK_VERDICTS = {
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename='program.log',
+    filename=Path('../program.log').resolve(),
     filemode='w',
-    format='%(asctime)s - %(levelname)s - %(message)s - %(name)s',
+    format='%(asctime)s - %(funcName)s - %(levelname)s - %(message)s',
 )
 logger = logging.getLogger(__name__)
-logger.addHandler(
-    logging.StreamHandler(),
-)
 
 
 class RequestExceptionError(Exception):
@@ -41,96 +43,173 @@ class TheAnswerIsNot200Error(Exception):
     """Ответ сервера не 200."""
 
 
+class TokenError(Exception):
+    """Отсутсвует один или более токен."""
+
+
 def check_tokens() -> bool:
-    """Проверка наличия токена."""
-    return all(
-        [PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID],
-    )
+    """Проверяем наличия токена.
+
+    Доступны ли токены, которые необходимы для работы программы.
+    Если хотя бы один токен не доступен , тогда программа прекрашает
+    свою работу.
+
+    Возвращает:
+    True если проверка пройдена или TokenError, если что-то отсутсвует.
+    """
+    list_tokens = []
+    for name in ['PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID']:
+        print(globals())
+        if globals()[name] is None:
+            list_tokens.append(name)
+    if len(list_tokens) == 0:
+        return True
+    else:
+        api_answer = 'Отстутствует одна или несколько переменных окружения'
+        logging.critical(api_answer)
+        raise TokenError
 
 
 def send_message(bot: telegram.bot.Bot, message: str) -> None:
-    """Отправка сообщения в Телеграм."""
+    """Отправляем сообщение в Телеграм.
+
+    В случае успеха пользователь получает сообщение в чат Телеграма.
+    В случае неудачи логируем в журнал запись об ошибке.
+
+    Параметры:
+    bot: экземпляр класса Bot.
+    message: строка сообщения с текстом.
+    """
+    logger.info(f'Вызываем функцию send_message c аргументами {bot, message}')
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logger.debug(
-            f'Сообщение в Telegram отправлено: {message}')
-    except Exception as telegram_error:
-        logger.error(
-            f'Сообщение в Telegram не отправлено: {telegram_error}')
+    except telegram.error.TelegramError as telegram_error:
+        logging.exception(
+            f'Сообщение в Telegram не отправлено: {telegram_error}',
+        )
+    logger.debug(
+            f'Сообщение в Telegram отправлено: {message}',
+    )
 
 
-def get_api_answer(timestamp: int) -> Dict[str, list]:
-    """Получение данных с API Яндекс Практикума."""
+def get_api_answer(timestamp: int) -> Dict[str, List[str]]:
+    """Получение данных с API Яндекс Практикума.
+
+    Делает запрос к единственному эндпойнту на предмет доступности.
+
+    Параметры:
+    timestamp: временная метка запроса.
+
+    Возвращает:
+    В случае успешного запроса возвращает ответ API в формате JSON.
+    В случае неудачи выводится ошибка и осуществляется запись в лог.
+    """
+    logger.info(f'Вызываем функцию get_api_answer c аргументами {timestamp}')
     try:
         response = requests.get(
             ENDPOINT,
             headers=HEADERS,
-            params=timestamp,
+            params={'from_date': timestamp},
         )
-        if response.status_code != 200:
-            api_answer = (
-                f'Эндпоинт {ENDPOINT} недоступен.'
-                f' Код ответа API: {response.status_code}'
-            )
-            logger.error(api_answer)
-            raise TheAnswerIsNot200Error(api_answer)
-        return response.json()
     except requests.exceptions.RequestException as request_error:
-        api_answer = f'Код ответа API : {request_error}'
-        logger.error(api_answer)
-        raise RequestExceptionError(api_answer) from request_error
+        logger.error(f'Код ответа API : {request_error}')
+        raise RequestExceptionError
+    if response.status_code != HTTPStatus.OK:
+        logger.error(
+            f'Эндпоинт {ENDPOINT} недоступен.'
+            f' Код ответа API: {response.status_code}',
+        )
+        raise TheAnswerIsNot200Error
+    return response.json()
 
 
-def check_response(response: Dict[str, list]) -> list:
-    """Проверяем данные в response."""
-    if isinstance(response, dict):
-        if 'homeworks' in response:
-            if isinstance(response.get('homeworks'), list):
-                return response.get('homeworks')
-            raise TypeError('API возвращает не список.')
-        raise KeyError('Не найден ключ homeworks.')
-    raise TypeError('API возвращает не словарь.')
+def check_response(response: Dict[str, List[str]]) -> List[str]:
+    """Проверяем данные в response.
+
+    Соответствует ли ответ API документации.
+
+    Параметры:
+    response: ответ API, приведенный к типам данных Python.
+
+    Возвращает:
+    В случае успеха список с информацией о всех выполненных домашних работах
+    и той , которая находятся в работе.
+    В случае неудачи обработки запроса выводится ошибка с просьбой
+    проверить переменные окружения.
+    """
+    logger.info(f'Вызываем функцию check_response c аргументами {response}')
+    if isinstance(response, dict) and all(
+        key for key in ('current_date', 'homeworks')
+    ) and isinstance(response.get('homeworks'), list):
+        return response.get('homeworks')
+    else:
+        raise TypeError(
+            'Ошибка API при проверке response, проверьте данные.',
+            )
 
 
 def parse_status(homework: Dict[str, str]) -> str:
-    """Анализируем статус если изменился."""
-    if isinstance(homework, dict):
-        if 'status' in homework:
-            if 'homework_name' in homework:
-                if isinstance(homework.get('status'), str):
-                    homework_name = homework.get('homework_name')
-                    homework_status = homework.get('status')
-                    if homework_status in HOMEWORK_VERDICTS:
-                        verdict = HOMEWORK_VERDICTS.get(homework_status)
-                        return ('Изменился статус проверки работы '
-                                f'"{homework_name}". {verdict}')
-                    else:
-                        raise Exception("Неизвестный статус работы")
-                raise TypeError('status не str.')
-            raise KeyError('В ответе нет ключа homework_name.')
-        raise KeyError('В ответе нет ключа status.')
-    raise KeyError('API возвращает не словарь.')
+    """Анализируем статус если изменился.
+
+    Извлекает статус о конкретной домашней работе.
+
+    Параметры:
+    homework: список с информацией о всех выполненных домашних работах
+    и той , которая находятся в работе.
+
+    Возвращает:
+    В случае если статус работы изменился , выводит сообщение об
+    изменении статуса работы.
+    В случае неудачи есть несколько сценариев выводимых ошибок в зависимости
+    от причины сбоя - соответствие значений по указываему ключу или
+    если статуса нет в словаре вердиктов.
+    """
+    logger.info(f'Вызываем функцию parse_status c аргументами {homework}')
+    try:
+        name, status = homework['homework_name'], homework['status']
+    except KeyError:
+        raise KeyError(
+            'Ошибка ключа при запросе названия и статуса домашней работы',
+            )
+    try:
+        verdict = HOMEWORK_VERDICTS[status]
+    except KeyError:
+        raise KeyError(
+                'Ошибка ключа при запросе статуса работы: ', verdict,
+                )
+    return f'Изменился статус проверки работы "{name}": {verdict}'
 
 
 def main() -> None:
-    """Основная логика работы бота."""
+    """Основная логика работы бота.
+
+    Делаем проверку токенов функцией check_tokens().
+    Запускаем бота.
+    Делаем запрос к API функцией get_api_answer().
+    Проверяем ответ функцией check_response().
+    Если есть обновления получаем статус обновления
+    функцией parse_status() и отправляем сообщение с уведомлением
+    в Telegram функцией send_message().
+    Далее уходим в режим ожидания на время RETRY_PERIOD и
+    возвращаемся к началу работы.
+
+    В случае сбоя в работе выводим в Telegram уведомление
+    с названием ошибки и логируем это в журнал.
+    """
     logging.info('Бот запущен.')
-    if not check_tokens():
-        api_answer = 'Отстутствует одна или несколько переменных окружения'
-        logger.critical(api_answer)
-        sys.exit(api_answer)
+    check_tokens()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     while True:
         try:
             response = get_api_answer(timestamp)
             homeworks = check_response(response)
-            logging.info('Cписок работ получен.')
-            if len(homeworks) > 0:
+            logger.info('Cписок работ получен.')
+            if homeworks:
                 send_message(bot, parse_status(homeworks[0]))
                 timestamp = response['current_date']
             else:
-                logging.info('Новых заданий нет.')
+                logger.info('Новых заданий нет.')
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             send_message(bot, message)
